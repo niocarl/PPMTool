992244944a674b5d01c97d1108b6a39e8becd47b
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2018-12-23 13:17:51 +0800

    first commit

---
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/PpmToolFullStackApplication.java
@@ -0,0 +1,14 @@
+package com.carlintelligence.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
+
