362d439438f6014cd6b1985109e5232af34a16ca
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2018-12-25 18:01:09 +0800

    Refactor Project Controller

--- PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/web/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlintelligence.ppmtool.domain.Project;
+import com.carlintelligence.ppmtool.services.MapValidationErrorService;
 import com.carlintelligence.ppmtool.services.ProjectService;
 
 @RestController
@@ -26,16 +27,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
-		if(result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			for(FieldError error : result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if (errorMap != null) return errorMap;
+		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
