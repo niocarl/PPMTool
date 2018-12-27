3ead0c625af7e5c9c11e5d220b9dea83e75b1882
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2018-12-27 23:12:02 +0800

    Add Boiler Plate react

--- PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/web/ProjectController.java
@@ -1,20 +1,16 @@
 package com.carlintelligence.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +57,6 @@ public class ProjectController {
 		projectService.deleteProjectByIdentifier(projectId);
 		
 		return new ResponseEntity<String>("Project with ID '" + projectId + "' was successfully deleted", HttpStatus.OK);
-	}
-	
+	}	
 
 }
