b66e0299a6023f23af4e35a4d8bc03dbcc95f6b8
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2019-01-04 13:51:46 +0800

    Persist project task part1

---
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,51 @@
+package com.carlintelligence.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlintelligence.ppmtool.domain.Backlog;
+import com.carlintelligence.ppmtool.domain.ProjectTask;
+import com.carlintelligence.ppmtool.repositories.BacklogRepository;
+import com.carlintelligence.ppmtool.repositories.ProjectTaskRepository;
+
+@Service
+public class ProjectTaskService {
+	
+	@Autowired
+	private BacklogRepository backlogRepository;
+	
+	@Autowired
+	private ProjectTaskRepository projectTaskRepository;
+	
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+		
+		//PT to be a specific project, project != null, BL exist
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		
+		//set the BL to PT
+		projectTask.setBacklog(backlog);
+		
+		//project sequence to be like this - IDPRO-1 IDPRO-2
+		Integer BacklogSequence = backlog.getPTSequence();
+		
+		//Update the BL sequence
+		BacklogSequence++;
+		
+		//Add sequence to PT
+		projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
+		
+		//INIITAL priority when priority is null
+		/*if(projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+			projectTask.setPriority(3);
+		}*/
+		
+		//INITIAL status when status is null
+		if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+			projectTask.setStatus("TO_DO");
+		}
+		
+		return projectTaskRepository.save(projectTask);
+	}
+
+}
