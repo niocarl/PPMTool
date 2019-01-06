0fb92b8003fb7623603d469de000c24763cd3c58
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2019-01-06 19:39:59 +0800

    Find projectTask by projectSequence w/ validation

--- PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/services/ProjectTaskService.java
@@ -74,9 +74,27 @@ public class ProjectTaskService {
 	
 	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 		
-		//make sure searching on the right backlog
+		//make sure searching on existing backlog
 		
-		return projectTaskRepository.findByProjectSequence(pt_id);
+		Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+		
+		if (backlog == null) {
+			throw new ProjectNotFoundException("Project with ID: '" + backlog_id +"' does not exist");
+		}
+		
+		//make sure that task exist
+		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+		
+		if (projectTask == null) {
+			throw new ProjectNotFoundException("Project Task with ID: '" + pt_id + "' not found");
+		}
+		
+		//make sure that the backlog/project id in the path corresponds to the right project
+		if(!projectTask.getProjectIdentifier().equals(backlog_id)) {
+			throw new ProjectNotFoundException("Project Task with ID: '" + pt_id + "' does not exist in project: '" + backlog_id);
+		}
+				
+		return projectTask;
 	}
 
 }
