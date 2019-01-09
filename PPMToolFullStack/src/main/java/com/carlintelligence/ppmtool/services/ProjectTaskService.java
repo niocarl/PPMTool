5f073cbfd57143375b86c9f28d8cd58fda97aae5
AWSYS-I\nino.balahadia <nino.balahadia@awsys-i.com>
2019-01-09 08:18:01 +0800

    Update validation and delete project task

--- PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/carlintelligence/ppmtool/services/ProjectTaskService.java
@@ -1,6 +1,8 @@
 package com.carlintelligence.ppmtool.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -96,5 +98,32 @@ public class ProjectTaskService {
 				
 		return projectTask;
 	}
+	
+	public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
+		
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+		
+		projectTask = updatedTask;
+		
+		return projectTaskRepository.save(projectTask);
+		
+		
+	}
+	
+	public void deletePTByProjectSequence(String backlog_id, String pt_id) {
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+		
+		Backlog backlog = projectTask.getBacklog();
+		List<ProjectTask> pts = backlog.getProjectTasks();
+		pts.remove(projectTask);
+		projectTaskRepository.delete(projectTask);
+	}
+		//update project task
+
+		//find existing project
+		
+		//replace it with updated task
+		
+		//save update
 
 }
